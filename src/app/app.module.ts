8b548b3264aaec50ca687bffa5f44fdd65345bbe
Mbote-Joseph <mbotejoseph001@gmail.com>
2020-10-26 18:56:43 +0300

    first commit of the full CRUD application using Angular Framework and using the json-server as the REST API

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeesListComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
